41f374be152fb94900fe4e3ab39289a44e341252
cccs-br <benoit.roy@cyber.gc.ca>
2023-11-10 09:53:53 -0500

    fixed copy published jars.

--- cccs-publish.sh
+++ cccs-publish.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build/jars
 cp ./spark/build/libs/*.jar build/jars
-cp ./spark/v3.4/spark/build/libs/*.jar build/jars
-cp ./spark/v3.4/spark-extensions/build/libs/*.jar build/jars
-cp ./spark/v3.4/spark-runtime/build/libs/*.jar build/jars
+cp ./spark/v3.5/spark/build/libs/*.jar build/jars
+cp ./spark/v3.5/spark-extensions/build/libs/*.jar build/jars
+cp ./spark/v3.5/spark-runtime/build/libs/*.jar build/jars
