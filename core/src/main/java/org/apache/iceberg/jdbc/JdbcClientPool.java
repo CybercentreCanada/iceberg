3623c8e43bfae351741bd97a5cbe5c7b8c5d775f
cccs-br <benoit.roy@cyber.gc.ca>
2024-09-09 11:55:58 -0400

    applying custom cccs mods

--- core/src/main/java/org/apache/iceberg/jdbc/JdbcClientPool.java
+++ core/src/main/java/org/apache/iceberg/jdbc/JdbcClientPool.java
@@ -48,7 +48,10 @@ public class JdbcClientPool extends ClientPoolImpl<Connection, SQLException> {
    * See https://en.wikipedia.org/wiki/SQLSTATE for more details.
    */
   static final Set<String> COMMON_RETRYABLE_CONNECTION_SQL_STATES =
-      ImmutableSet.of("08000", "08003", "08006", "08007", "40001");
+      // CCCS modification: starts
+      // ImmutableSet.of("08000", "08003", "08006", "08007", "40001");
+      ImmutableSet.of("08000", "08003", "08006", "08004", "08006", "08007", "40001", "57000");
+      // CCCS modification: end
 
   private final String dbUrl;
   private final Map<String, String> properties;
