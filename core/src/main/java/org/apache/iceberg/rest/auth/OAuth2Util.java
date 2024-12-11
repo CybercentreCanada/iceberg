d47705ef23e0d06ce5bf965042cb3174124c9c79
cccs-nik <68961854+cccs-nik@users.noreply.github.com>
2024-12-11 17:17:18 -0500

    im being trolled

--- core/src/main/java/org/apache/iceberg/rest/auth/OAuth2Util.java
+++ core/src/main/java/org/apache/iceberg/rest/auth/OAuth2Util.java
@@ -572,7 +572,8 @@ public class OAuth2Util {
       // Exchange flow for credentials flow tokens doesn't work with Azure,
       // we're just supposed to fetch a brand new token with the same credentials.
       if (credential() != null) {
-        return fetchToken(client, headers(), credential(), scope(), oauth2ServerUri(), optionalOAuthParams());
+        return fetchToken(
+            client, headers(), credential(), scope(), oauth2ServerUri(), optionalOAuthParams());
       }
       // END CCCS EDIT
 
