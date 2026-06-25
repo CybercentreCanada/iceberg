82f5c4d5408774ee67862348c73074d1675c5022
cccs-nik <68961854+cccs-nik@users.noreply.github.com>
2026-06-25 18:13:16 -0400

    cccs changes

--- core/src/main/java/org/apache/iceberg/SnapshotScan.java
+++ core/src/main/java/org/apache/iceberg/SnapshotScan.java
@@ -148,9 +148,7 @@ public abstract class SnapshotScan<ThisT, T extends ScanTask, G extends ScanTask
                   .projectedFieldNames(projectedFieldNames)
                   .tableName(table().name())
                   .snapshotId(snapshot.snapshotId())
-                  .filter(
-                      ExpressionUtil.sanitize(
-                          schema().asStruct(), filter(), context().caseSensitive()))
+                  .filter(filter())
                   .scanMetrics(ScanMetricsResult.fromScanMetrics(scanMetrics()))
                   .metadata(metadata)
                   .build();
--- core/src/main/java/org/apache/iceberg/jdbc/JdbcClientPool.java
+++ core/src/main/java/org/apache/iceberg/jdbc/JdbcClientPool.java
@@ -48,7 +48,7 @@ public class JdbcClientPool extends ClientPoolImpl<Connection, SQLException> {
    * See https://en.wikipedia.org/wiki/SQLSTATE for more details.
    */
   static final Set<String> COMMON_RETRYABLE_CONNECTION_SQL_STATES =
-      ImmutableSet.of("08000", "08003", "08006", "08007", "40001");
+      ImmutableSet.of("08000", "08003", "08006", "08004", "08006", "08007", "40001", "57000");
 
   private final String dbUrl;
   private final Map<String, String> properties;
