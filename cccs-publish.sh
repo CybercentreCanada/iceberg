9138b357e3cc1967645ea5a8924d0bd6537b0584
Zachary Wilkins <zachary.wilkins@cyber.gc.ca>
2026-07-30 15:18:01 -0400

    deps(SPBK-4151): target Spark 4.1

--- cccs-publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p build/jars
-cp ./spark/build/libs/*.jar build/jars
-cp ./spark/v3.5/spark/build/libs/*.jar build/jars
-cp ./spark/v3.5/spark-extensions/build/libs/*.jar build/jars
-cp ./spark/v3.5/spark-runtime/build/libs/*.jar build/jars
